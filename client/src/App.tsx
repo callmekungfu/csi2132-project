b41d70e30e2c0a270599deeb5ef0e491afe7f1e2
Keith Tang <keithtang28@gmail.com>
2021-03-28 18:41:14 -0400

    dough

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-// import { Router } from 'react-router';
+import { Router } from 'react-router';
 import HomePage from './components/pages/HomePage';
 
 function App() {
   return (
-    <div>
+    <Router>
       <HomePage/>
-    </div>
+    </Router>
   );
 }
 
