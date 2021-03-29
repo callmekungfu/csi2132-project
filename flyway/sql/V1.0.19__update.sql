e5587e54e5480a0bb2f05edefc7dcdfe74aa351c
Yong Lin Wang <wangyonglin1999@gmail.com>
2021-03-28 21:29:12 -0400

    feat(db): more sanity triggers

--- flyway/sql/V1.0.19__update.sql
+++ flyway/sql/V1.0.19__update.sql
@@ -18,3 +18,62 @@ create trigger check_booking_times
 for each row
     execute procedure check_booking_date_before_insert();
 
+create or replace function check_if_last_office_email()
+    returns trigger as
+$$
+    begin
+        if (select count(*)
+        from office_emails oe
+        where oe.hotel_brand_id = old.hotel_brand_id) = 1 then
+            raise exception 'This is the last email for the brand, it cannot be removed';
+        end if;
+        return old;
+    end;
+$$ language plpgsql;
+
+drop trigger if exists check_if_last_office_email ON office_emails;
+
+create trigger check_if_last_office_email
+    before delete on office_emails
+for each row
+    execute procedure check_if_last_office_email();
+
+create or replace function check_if_last_office_phone()
+    returns trigger as
+$$
+    begin
+        if (select count(*)
+        from office_phones oe
+        where oe.hotel_brand_id = old.hotel_brand_id) = 1 then
+            raise exception 'This is the last phone for the brand, it cannot be removed';
+        end if;
+        return old;
+    end;
+$$ language plpgsql;
+
+drop trigger if exists check_if_last_office_phone ON office_phones;
+
+create trigger check_if_last_office_phone
+    before delete on office_phones
+for each row
+    execute procedure check_if_last_office_phone();
+
+create or replace function check_if_last_hotel_phone()
+    returns trigger as
+$$
+    begin
+        if (select count(*)
+        from hotel_phones oe
+        where oe.hotel_id = old.hotel_id) = 1 then
+            raise exception 'This is the last phone for the hotel, it cannot be removed';
+        end if;
+        return old;
+    end;
+$$ language plpgsql;
+
+drop trigger if exists check_if_last_hotel_phone ON hotel_phones;
+
+create trigger check_if_last_hotel_phone
+    before delete on hotel_phones
+for each row
+    execute procedure check_if_last_hotel_phone();
